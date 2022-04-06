63b14de5a24ff2a1889883545438d44643f59f97
NianZo <nicholas.angle@gmail.com>
2022-04-05 21:51:52 -0700

    Add code from chapter 5 Command Buffers and Memory Management

---
+++ chapter_3/Handshake/include/Wrapper.hpp
@@ -0,0 +1,29 @@
+/*
+ * Wrapper.hpp
+ *
+ *  Created on: Apr 5, 2022
+ *      Author: nic
+ */
+
+#ifndef CHAPTER_3_HANDSHAKE_INCLUDE_WRAPPER_HPP_
+#define CHAPTER_3_HANDSHAKE_INCLUDE_WRAPPER_HPP_
+
+#include "Header.hpp"
+
+class CommandBufferMgr
+{
+	// Allocate memory for command buffers from the command pool
+	static void allocCommandBuffer(const VkDevice* device, const VkCommandPool cmdPool, VkCommandBuffer* cmdBuf, const VkCommandBufferAllocateInfo* commandBufferInfo);
+
+	// Start command buffer recording
+	static void beginCommandBuffer(VkCommandBuffer cmdBuf, VkCommandBufferBeginInfo* inCmdBufInfo = nullptr);
+
+	// End the command buffer recording
+	static void endCommandBuffer(VkCommandBuffer cmdBuf);
+
+	// Submit the command buffer for execution
+	static void submitCommandBuffer(const VkQueue& queue, const VkCommandBuffer* cmdBufList, const VkSubmitInfo* submitInfo = nullptr, const VkFence& fence = VK_NULL_HANDLE);
+};
+
+
+#endif /* CHAPTER_3_HANDSHAKE_INCLUDE_WRAPPER_HPP_ */
---
+++ chapter_3/Handshake/source/Wrapper.cpp
@@ -0,0 +1,124 @@
+/*
+ * Wrapper.cpp
+ *
+ *  Created on: Apr 5, 2022
+ *      Author: nic
+ */
+
+#include "Wrapper.hpp"
+
+void CommandBufferMgr::allocCommandBuffer(const VkDevice* device, const VkCommandPool cmdPool, VkCommandBuffer* cmdBuf, const VkCommandBufferAllocateInfo* commandBufferInfo)
+{
+	VkResult result;
+	// If command information is available use it as it is
+	if (commandBufferInfo)
+	{
+		result = vkAllocateCommandBuffers(*device, commandBufferInfo, cmdBuf);
+		assert(!result);
+		return;
+	}
+
+	// Default implementation, create the command buffer allocation info
+	// and use the supplied parameter into it
+	VkCommandBufferAllocateInfo cmdInfo;
+	cmdInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+	cmdInfo.pNext = nullptr;
+	cmdInfo.commandPool = cmdPool;
+	cmdInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+	cmdInfo.commandBufferCount = (uint32_t) sizeof(cmdBuf) / sizeof(VkCommandBuffer);
+	result = vkAllocateCommandBuffers(*device, &cmdInfo, cmdBuf);
+	assert(!result);
+}
+
+void CommandBufferMgr::beginCommandBuffer(VkCommandBuffer cmdBuf, VkCommandBufferBeginInfo* inCmdBufInfo)
+{
+	VkResult result;
+	// If the user has specified the custom command buffer use it
+	if (inCmdBufInfo)
+	{
+		result = vkBeginCommandBuffer(cmdBuf, inCmdBufInfo);
+		assert(result == VK_SUCCESS);
+		return;
+	}
+
+	// otherwise, use the default implementation
+	VkCommandBufferInheritanceInfo cmdBufInheritInfo;
+	cmdBufInheritInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_INHERITANCE_INFO;
+	cmdBufInheritInfo.pNext = nullptr;
+	cmdBufInheritInfo.renderPass = VK_NULL_HANDLE;
+	cmdBufInheritInfo.subpass = 0;
+	cmdBufInheritInfo.framebuffer = VK_NULL_HANDLE;
+	cmdBufInheritInfo.occlusionQueryEnable = VK_FALSE;
+	cmdBufInheritInfo.queryFlags = 0;
+	cmdBufInheritInfo.pipelineStatistics = 0;
+	VkCommandBufferBeginInfo cmdBufInfo;
+	cmdBufInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+	cmdBufInfo.pNext = nullptr;
+	cmdBufInfo.flags = 0;
+	cmdBufInfo.pInheritanceInfo = &cmdBufInheritInfo;
+
+	result = vkBeginCommandBuffer(cmdBuf, &cmdBufInfo);
+	assert(result == VK_SUCCESS);
+}
+
+void CommandBufferMgr::endCommandBuffer(VkCommandBuffer cmdBuf)
+{
+	VkResult result;
+	result = vkEndCommandBuffer(cmdBuf);
+	assert(result == VK_SUCCESS);
+}
+
+void CommandBufferMgr::submitCommandBuffer(const VkQueue& queue, const VkCommandBuffer* cmdBufList, const VkSubmitInfo* submitInfo, const VkFence& fence)
+{
+	VkResult result;
+	// If submit information is available use it as it is.
+	// This assumes that the commands are already specified
+	// in the structure, hence ignore command buffer
+	if (submitInfo)
+	{
+		result = vkQueueSubmit(queue, 1, submitInfo, fence);
+		assert(!result);
+		result = vkQueueWaitIdle(queue);
+		assert(!result);
+		return;
+	}
+
+	// else create the submit info with specified buffer commands
+	VkSubmitInfo submitInfoLocal;
+	submitInfoLocal.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+	submitInfoLocal.pNext = nullptr;
+	submitInfoLocal.waitSemaphoreCount = 0;
+	submitInfoLocal.pWaitSemaphores = nullptr;
+	submitInfoLocal.pWaitDstStageMask = nullptr;
+	submitInfoLocal.commandBufferCount = (uint32_t) sizeof(cmdBufList) / sizeof(VkCommandBuffer);
+	submitInfoLocal.pCommandBuffers = cmdBufList;
+	submitInfoLocal.signalSemaphoreCount = 0;
+	submitInfoLocal.pSignalSemaphores = nullptr;
+
+	result = vkQueueSubmit(queue, 1, &submitInfoLocal, fence);
+	assert(!result);
+	result = vkQueueWaitIdle(queue);
+	assert(!result);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
